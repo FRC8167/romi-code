0b4cacc58336b3a7925340df5f64a296a7d0a643
tjf801 <fuscofamily0@gmail.com>
2021-02-25 18:09:27 -0500

    tabs not spaces

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -9,31 +9,31 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class ArcadeDrive extends CommandBase {
-  private final Drivetrain drivetrain;
-  private final Joystick controller;
+	private final Drivetrain drivetrain;
+	private final Joystick controller;
 
-  public ArcadeDrive(Drivetrain drivetrain, Joystick controller) {
-    this.drivetrain = drivetrain;
-    this.controller = controller;
-  }
+	public ArcadeDrive(Drivetrain drivetrain, Joystick controller) {
+		this.drivetrain = drivetrain;
+		this.controller = controller;
+	}
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    drivetrain.arcadeDrive(-controller.getRawAxis(1), controller.getRawAxis(2));
-  }
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		drivetrain.arcadeDrive(-controller.getRawAxis(1), controller.getRawAxis(2));
+	}
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
 }
--- src/main/java/frc/robot/commands/AutonomousDistance.java
+++ src/main/java/frc/robot/commands/AutonomousDistance.java
@@ -8,17 +8,17 @@ import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class AutonomousDistance extends SequentialCommandGroup {
-  /**
-   * Creates a new Autonomous Drive based on distance. This will drive out for a specified distance,
-   * turn around and drive back.
-   *
-   * @param drivetrain The drivetrain subsystem on which this command will run
-   */
-  public AutonomousDistance(Drivetrain drivetrain) {
-    addCommands(
-        new DriveDistance(-0.5, 10, drivetrain),
-        new TurnDegrees(-0.5, 90, drivetrain),
-        new DriveDistance(-0.5, 10, drivetrain),
-        new TurnDegrees(0.5, 90, drivetrain));
-  }
+	/**
+	 * Creates a new Autonomous Drive based on distance. This will drive out for a specified distance,
+	 * turn around and drive back.
+	 *
+	 * @param drivetrain The drivetrain subsystem on which this command will run
+	 */
+	public AutonomousDistance(Drivetrain drivetrain) {
+		addCommands(
+				new DriveDistance(-0.5, 10, drivetrain),
+				new TurnDegrees(-0.5, 90, drivetrain),
+				new DriveDistance(-0.5, 10, drivetrain),
+				new TurnDegrees(0.5, 90, drivetrain));
+	}
 }
